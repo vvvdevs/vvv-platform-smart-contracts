b9d63d08fe2d36ce69bfe0fa4a958f0e81f2f62f
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2023-06-03 22:31:54 -0400

    added rest of framework

---
+++ scripts/Deploy.s.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.8.19;
+
+/**
+forge script scripts/Deploy.s.sol:DeployLockScript --private-key $PRIVATE_KEY --fork-url $ETH_GOERLI_TESTNET_URL
+ */
+
+
+import {Lock} from "contracts/Lock.sol";
+import "lib/forge-std/src/Script.sol";
+
+contract DeployLockScript is Script {
+
+    Lock public lock;
+
+    function run() public {
+
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+            lock = new Lock(block.timestamp + 1 days);
+
+        vm.stopBroadcast();
+
+        console.log("Deployed Lock contract to address: ", address(lock));
+
+    }
+
+}
