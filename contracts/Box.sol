1361965fc7d1b09ab79792a1f76f782075c9e142
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2023-06-06 11:45:13 -0400

    PR2 requested changes

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -12,10 +12,6 @@ proxy admin
 contract Box {
     uint public val;
 
-    // constructor(uint _val) {
-    //     val = _val;
-    // }
-
     function initialize(uint _val) external {
         val = _val;
     }
--- contracts/BoxV2.sol
+++ contracts/BoxV2.sol
@@ -4,10 +4,6 @@ pragma solidity 0.8.19;
 contract BoxV2 {
     uint public val;
 
-    // function initialize(uint _val) external {
-    //     val = _val;
-    // }
-
     function inc() external {
         val += 1;
     }
