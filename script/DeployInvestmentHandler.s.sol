0d3e8a32360c7bb301c125c1b4ef1e2760e6617d
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2023-08-20 00:46:01 -0400

    added deploy script for InvestmentHandler

---
+++ script/DeployInvestmentHandler.s.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {InvestmentHandler} from "contracts/InvestmentHandler.sol";
+import "forge-std/Script.sol";
+
+contract DeployInvestmentHandler is Script {
+
+    InvestmentHandler public investmentHandler;
+    
+    // Admin Role Addresses: Testing is msg.sender, Production is a multisig or hardware wallet for each role
+    address public defaultAdminController = msg.sender;
+    address public pauser = msg.sender;
+    address public investmentManager = msg.sender;
+    address public contributionAndRefundManager = msg.sender;
+    address public refunder = msg.sender;
+
+    function run() public {
+        investmentHandler = new InvestmentHandler(
+            defaultAdminController,
+            pauser,
+            investmentManager,
+            contributionAndRefundManager,
+            refunder
+        );
+
+        console.log("InvestmentHandler deployed at address: %s", address(investmentHandler));
+    }
+}
+
+/**
+    forge script script/DeployInvestmentHandler.s.sol:DeployInvestmentHandler --fork-url $ETH_GOERLI_URL --private-key $PRIVATE_KEY --etherscan-api-key $ETHERSCAN_API_KEY 
+    
+    (optinally) --broadcast --verify
+ */
