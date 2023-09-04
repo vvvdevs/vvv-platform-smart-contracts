45337733dd0a4110c0ee0ad7a9d907ccd3ec3b39
Rik van der Werf <rikvanderwerf@coderstudio.nl>
2023-09-04 20:26:07 +0200

    Fixed VVVToken constructor issue

--- contracts/tokens/VvvToken.sol
+++ contracts/tokens/VvvToken.sol
@@ -10,12 +10,11 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20Capped } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract VVVToken is ERC20, Ownable, ERC20Capped {
+contract VVVToken is ERC20Capped, Ownable{
     event tokensMinted (address indexed to, uint256 amount);
 
     constructor(uint256 _cap, uint256 _initialSupply) ERC20("vVvToken", "VVV") ERC20Capped(_cap) {
         _mint(msg.sender, _initialSupply);
-
         emit tokensMinted(msg.sender, _initialSupply);
     }
 
