83a05c08c5d278d5b1547773457e2bcf8dd9b4ab
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2023-10-16 15:10:42 -0400

    Converted to ERC721A, cleaned up functions, single signature for multiple whitelist mints

--- contracts/FundNFT.sol
+++ contracts/FundNFT.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import { ERC721A } from "erc721a/contracts/ERC721A.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
@@ -9,97 +9,119 @@ import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract VVV_FUND is ERC1155, AccessControl, ReentrancyGuard {
+contract VVV_FUND is ERC721A, AccessControl, ReentrancyGuard {
 
     IERC721 public immutable S1NFT;
     address public signer;
-    address public custodian;
-    string public name;
-    string public symbol;
-    mapping(bytes => bool) public signatureHasBeenUsed;
+    string public baseURI;
+    uint256 public constant MAX_SUPPLY = 10_000;
+    mapping(address => uint256) public mintedBySignature;
 
-    error InvalidSignature();
-    error SignatureHasBeenUsed();
     error ArrayLengthMismatch();
+    error InvalidSignature();
+    error MaxAllocationWouldBeExceeded();
+    error MaxSupplyWouldBeExceeded();
+    error NotTokenOwner();
 
     constructor(
         address _s1nft,
         address _signer,
-        address _custodian,
         string memory _name,
         string memory _symbol,
-        string memory _uri
-    ) ERC1155(_uri) {
+        string memory _baseUri
+    ) ERC721A(_name, _symbol) {
         S1NFT = IERC721(_s1nft);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         signer = _signer;
-        custodian = _custodian;
-        name = _name;
-        symbol = _symbol;
+        setBaseURI(_baseUri);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721A, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
     //==================================================================================================
-    // MINTING BY TRADING IN PREVIOUS NFT COLLECTION
-    // inbuilt checks confirm that the S1NFT is owned by the msg.sender and that this contract has permission to transfer it on behalf of the user
-    // this function is only callable by the owner of the S1NFT
-    // S1NFT are sent to this contract- could also be 0xdead if desired - original contract does not include burn function
-    function batchMintByTradeIn(
-        address _to,
-        uint256[] memory _ids
-    ) public nonReentrant {
-        for(uint256 i = 0; i < _ids.length; i++) {
-            mintByTradeIn(_ids[i]);
-        }
-    }
-    
-    function mintByTradeIn(
-        uint256 _id
-    ) public nonReentrant {
-        S1NFT.transferFrom(msg.sender, address(this), _id);
-        _mint(custodian, _id, 1, "");
-    }
-
+    // MINTING BY TRADING IN PREVIOUS NFT COLLECTION (MIGRATION)
     //==================================================================================================
-    // MINTING BY SIGNATURE
-    function batchMintBySignature(
+    /**
+        @notice inbuilt checks confirm that the S1NFT is owned by the msg.sender and that this contract has permission to transfer it on behalf of the user    
+        @notice this function is only callable by the owner of the S1NFT
+        @notice S1NFT are sent to this contract- could also be 0xdead if desired - original contract does not include burn function
+        @notice requires this contract to be approved to transfer S1NFT on behalf of the user
+     */ 
+    function mintByTradeIn(
         address _to,
         uint256[] memory _ids,
-        bytes[] memory _signatures
+        uint256 _quantity
     ) public nonReentrant {
-        if(_ids.length != _signatures.length) {
+        if(_ids.length != _quantity){
             revert ArrayLengthMismatch();
         }
 
-        for(uint256 i = 0; i < _ids.length; i++) {
-            mintBySignature(_ids[i], _signatures[i]);
+        for(uint256 i = 0; i < _ids.length; ++i) {
+            if(S1NFT.ownerOf(_ids[i]) != msg.sender) {
+                revert NotTokenOwner();
+            }
+            S1NFT.transferFrom(msg.sender, address(this), _ids[i]);
         }
+
+        _safeMint(_to, _quantity);
     }
 
+    //==================================================================================================
+    // MINTING BY SIGNATURE (WHITELIST)
+    //==================================================================================================
+    
+    /**
+     * @notice mints via signature while ensuring the max mintable amount specified in the signature is not exceeded
+     */
     function mintBySignature(
-        uint256 _id,
+        address _to,
+        uint256 _quantity,
+        uint256 _maxQuantity,
         bytes memory _signature
-    ) public nonReentrant {
+    ) external payable nonReentrant {
 
-        if(!_signatureCheck(msg.sender, _id, _signature)) {
+        if(!_isSignatureValid(msg.sender, _maxQuantity, _signature)) {
             revert InvalidSignature();
         }
 
-        if(signatureHasBeenUsed[_signature]) {
-            revert SignatureHasBeenUsed();
+        if(_quantity + totalSupply() > MAX_SUPPLY) {
+            revert MaxSupplyWouldBeExceeded();
         }
 
-        signatureHasBeenUsed[_signature] = true;
+        if(mintedBySignature[msg.sender] + _quantity > _maxQuantity) {
+            revert MaxAllocationWouldBeExceeded();
+        }
 
-        _mint(custodian, _id, 1, "");
+        mintedBySignature[msg.sender] += _quantity;
+
+        _mint(_to, _quantity);
     }
 
-    function _signatureCheck(
+    //==================================================================================================
+    // ADMIN FUNCTIONS
+    //==================================================================================================
+    function adminMint(
+        address _to,
+        uint256 _quantity
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if(_quantity + totalSupply() > MAX_SUPPLY) {
+            revert MaxSupplyWouldBeExceeded();
+        }
+        _mint(_to, _quantity);
+    }
+
+    function setBaseURI(string memory _uri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = _uri;
+    }
+
+    //==================================================================================================
+    // INTERNAL FUNCTIONS
+    //==================================================================================================
+    function _isSignatureValid(
         address _minter,
-        uint256 _id,
+        uint256 _maxQuantity,
         bytes memory _signature
     ) private view returns (bool) {
         return
@@ -109,13 +131,24 @@ contract VVV_FUND is ERC1155, AccessControl, ReentrancyGuard {
                     keccak256(
                         abi.encodePacked(
                             _minter,
-                            _id,
+                            _maxQuantity,
                             block.chainid
                         )
                     )
                 ),
                 _signature
             );
+    }
+
+    //==================================================================================================
+    // OVERRIDES
+    //==================================================================================================
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
     }
 
 }
