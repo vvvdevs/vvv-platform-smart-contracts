345148d8bed5a4c251dc4f3be3577eecda91d82b
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2024-01-12 12:32:38 -0500

    formatted .sol files, set up pre-commit hooks, cleaned up .gitignore and .prettierignore, removed explicitTypes

--- test/vesting/VVVVesting.fuzz.t.sol
+++ test/vesting/VVVVesting.fuzz.t.sol
@@ -1,21 +1,17 @@
 //SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {VVVVestingTestBase} from "test/vesting/VVVVestingTestBase.sol";
+import {MockERC20} from "contracts/mock/MockERC20.sol";
+import {VVVVesting} from "contracts/vesting/VVVVesting.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 /**
  * @title VVVVesting Fuzz Tests
  * @dev use "forge test --match-contract VVVVestingFuzzTests" to run tests
  * @dev use "forge coverage --match-contract VVVVestingFuzzTests" to run coverage
  */
-pragma solidity ^0.8.23;
-
-import { VVVVestingTestBase } from "test/vesting/VVVVestingTestBase.sol";
-import { MockERC20 } from "contracts/mock/MockERC20.sol";
-import { VVVVesting } from "contracts/vesting/VVVVesting.sol";
-import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
-
 contract VVVVestingFuzzTests is VVVVestingTestBase {
-    //=====================================================================
-    // SETUP
-    //=====================================================================
     function setUp() public {
         vm.startPrank(deployer, deployer);
 
@@ -27,25 +23,21 @@ contract VVVVestingFuzzTests is VVVVestingTestBase {
         vm.stopPrank();
     }
 
-    //=====================================================================
-    // FUZZ TESTS
-    //=====================================================================
     //test setting a vesting schedule and withdrawing tokens, assert that the correct amount of tokens are withdrawn
     //fuzzes with withdraw values between 0 and (vested-withdrawn)
     function testFuzz_WithdrawVestedTokens(uint256 _tokenAmountToWithdraw) public {
         uint256 vestingScheduleIndex = 0;
         uint256 totalAmount = 10_000 * 1e18; //10k tokens
         uint256 amountWithdrawn = 0;
-        uint256 duration = 120; 
+        uint256 duration = 120;
         uint256 startTime = block.timestamp;
 
-        setVestingScheduleFromDeployer(sampleUser, vestingScheduleIndex, totalAmount, amountWithdrawn, duration, startTime);
+        setVestingScheduleFromDeployer(
+            sampleUser, vestingScheduleIndex, totalAmount, amountWithdrawn, duration, startTime
+        );
 
         uint256 vestedAmount = VVVVestingInstance.getVestedAmount(sampleUser, vestingScheduleIndex);
-        (, uint256 withdrawnTokens, , ) = VVVVestingInstance.userVestingSchedules(
-            sampleUser,
-            vestingScheduleIndex
-        );
+        (, uint256 withdrawnTokens,,) = VVVVestingInstance.userVestingSchedules(sampleUser, vestingScheduleIndex);
         uint256 amountToWithdraw = Math.min(vestedAmount - withdrawnTokens, _tokenAmountToWithdraw);
 
         withdrawVestedTokensAsUser(sampleUser, amountToWithdraw, sampleUser, vestingScheduleIndex);
@@ -56,21 +48,20 @@ contract VVVVestingFuzzTests is VVVVestingTestBase {
     function testFuzz_GetVestedAmount(address _vestedUser, uint8 _vestingTime) public {
         uint256 totalAmount = 10_000 * 1e18; //10k tokens
         uint256 amountWithdrawn = 0;
-        uint256 duration = 120; 
+        uint256 duration = 120;
         uint256 startTime = block.timestamp;
         uint256 vestingScheduleIndex = 0;
 
-        setVestingScheduleFromDeployer(_vestedUser, vestingScheduleIndex, totalAmount, amountWithdrawn, duration, startTime);
-        
+        setVestingScheduleFromDeployer(
+            _vestedUser, vestingScheduleIndex, totalAmount, amountWithdrawn, duration, startTime
+        );
+
         uint256 vestingTime = _vestingTime > 0 ? _vestingTime : 1;
         advanceBlockNumberAndTimestampInSeconds(vestingTime);
 
         uint256 vestedAmount = VVVVestingInstance.getVestedAmount(_vestedUser, vestingScheduleIndex);
 
-        uint256 referenceVestedAmount = Math.min(
-            totalAmount,
-            (totalAmount * (block.timestamp - startTime)) / duration
-        );
+        uint256 referenceVestedAmount = Math.min(totalAmount, (totalAmount * (block.timestamp - startTime)) / duration);
 
         assertEq(vestedAmount, referenceVestedAmount);
     }
