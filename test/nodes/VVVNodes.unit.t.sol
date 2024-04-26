c3c479a944f36a9af8a70f02bef83a2e9e3a76bc
curion <81062335+curi0n-s@users.noreply.github.com>
2024-04-26 04:19:47 +0000

    added functionality + tests for claiming vested

--- test/nodes/VVVNodes.unit.t.sol
+++ test/nodes/VVVNodes.unit.t.sol
@@ -1,13 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import { VVVAuthorizationRegistry } from "contracts/auth/VVVAuthorizationRegistry.sol";
 import { VVVNodes } from "contracts/nodes/VVVNodes.sol";
 import { VVVNodesTestBase } from "./VVVNodesTestBase.sol";
+import { VVVToken } from "contracts/tokens/VvvToken.sol";
 
 contract VVVNodesUnitTest is VVVNodesTestBase {
     function setUp() public {
         vm.startPrank(deployer, deployer);
-        NodesInstance = new VVVNodes();
+        AuthRegistry = new VVVAuthorizationRegistry(defaultAdminTransferDelay, deployer);
+        VVVTokenInstance = new VVVToken(type(uint256).max, 0, address(AuthRegistry));
+        NodesInstance = new VVVNodes(address(VVVTokenInstance));
+        VVVTokenInstance.mint(address(NodesInstance), 100_000_000 * 1e18);
         vm.stopPrank();
     }
 
@@ -32,5 +37,30 @@ contract VVVNodesUnitTest is VVVNodesTestBase {
         NodesInstance.setTokenURI(1, "https://example.com/token/1");
         vm.stopPrank();
         assertEq(NodesInstance.tokenURI(1), "https://example.com/token/1");
+    }
+
+    //tests claim
+    function testClaim() public {
+        vm.startPrank(sampleUser, sampleUser);
+        NodesInstance.mint(sampleUser); //mints token of ID 1
+        uint256 userTokenId = 1;
+
+        //check pre-vesting unvested tokens and owner wallet balance for userTokenId
+        (uint256 unvestedAmountPreClaim, , , ) = NodesInstance.tokenData(userTokenId);
+        uint256 balancePreClaim = VVVTokenInstance.balanceOf(sampleUser);
+
+        //activate node, and wait for tokens to vest
+        NodesInstance.activateNode(userTokenId);
+        advanceBlockNumberAndTimestampInSeconds(104 weeks); //2 years
+        NodesInstance.claim(userTokenId);
+
+        //check post-vesting unvested tokens and owner wallet balance for userTokenId
+        (uint256 unvestedAmountPostClaim, , , ) = NodesInstance.tokenData(userTokenId);
+        uint256 balancePostClaim = VVVTokenInstance.balanceOf(sampleUser);
+
+        vm.stopPrank();
+
+        //the change in unvested tokens should be equal to the change in balance
+        assertEq(unvestedAmountPreClaim - unvestedAmountPostClaim, balancePostClaim - balancePreClaim);
     }
 }
--- test/nodes/VVVNodesTestBase.sol
+++ test/nodes/VVVNodesTestBase.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.23;
 
 import { Test } from "lib/forge-std/src/Test.sol";
+import { VVVAuthorizationRegistry } from "contracts/auth/VVVAuthorizationRegistry.sol";
 import { VVVNodes } from "contracts/nodes/VVVNodes.sol";
+import { VVVToken } from "contracts/tokens/VvvToken.sol";
 
 abstract contract VVVNodesTestBase is Test {
+    VVVAuthorizationRegistry AuthRegistry;
     VVVNodes NodesInstance;
+    VVVToken VVVTokenInstance;
 
     uint256 deployerKey = 1234;
     uint256 sampleUserKey = 1234567;
@@ -15,6 +19,8 @@ abstract contract VVVNodesTestBase is Test {
 
     uint256 blockNumber;
     uint256 blockTimestamp;
+
+    uint48 defaultAdminTransferDelay = 1 days;
 
     function advanceBlockNumberAndTimestampInBlocks(uint256 blocks) public {
         blockNumber += blocks;
